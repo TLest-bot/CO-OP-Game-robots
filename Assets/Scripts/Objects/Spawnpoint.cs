babf7219aaef46c28620ffb5db3186f2985fe012
Ihavenojoy <tom.van.de.lest@gmail.com>
2026-03-16 12:06:24 +0100

    Start Spawnpoint Machenics

---
+++ Assets/Scripts/Objects/Spawnpoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Objects
+{
+    public class Spawnpoint
+    {
+        public int level;
+        public bool unlocked;
+        public Vector2 location;
+    }
+}
---
+++ Assets/Scripts/SpawnPointManager.cs
@@ -0,0 +1,106 @@
+using Assets.Scripts.Objects;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnPointManager : MonoBehaviour
+{
+    public List<Spawnpoint> Spawnpoints;
+    public GameObject objects;
+    void Start()
+    {
+        Spawnpoint spawn = new();
+        spawn.unlocked = false;
+        spawn.location = objects.transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public Spawnpoint GetSpawnpointByLevel(int targetLevel)
+    {
+        // Zoek het eerste spawnpoint dat matcht met het targetLevel
+        Spawnpoint foundPoint = Spawnpoints.FirstOrDefault(s => s.level == targetLevel);
+
+        if (foundPoint != null)
+        {
+            return foundPoint;
+        }
+        else
+        {
+            Debug.LogWarning($"Level {targetLevel} niet gevonden in de spawnpoints lijst!");
+            return null;
+        }
+    }
+    public bool isplayer(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            GameObject player = this.gameObject;
+        }
+        {
+            return true;
+        }
+        return false;
+    }
+    public void Teleport(GameObject player,  Spawnpoint spawnpoint)
+    {
+        player.transform.position = spawnpoint.location;
+    }
+
+    void ZeroButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(0)); }
+        Debug.Log("button one pressed");
+    }
+    void OneButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(1)); }
+        Debug.Log("button one pressed");
+    }
+
+    void TwoButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(2)); }
+    }
+
+    void ThreeButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(3)); }
+    }
+
+    void FourButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(4)); }
+    }
+
+    void FiveButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(5)); }
+    }
+
+    void SixButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(6)); }
+    }
+
+    void SevenButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(7)); }
+    }
+
+    void EightButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(8)); }
+    }
+
+    void NineButton(InputAction.CallbackContext context)
+    {
+        if (context.performed) { Teleport(this.gameObject, GetSpawnpointByLevel(9)); }
+    }
+}
